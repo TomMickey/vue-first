fa48d39c18f62082ea4f88ce15b44961c341a7e8
18620243154@163.com <2019@grg>
2022-08-22 17:24:03 +0800

    提交修改

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,19 @@
   <div>
     <el-empty description="描述文字"></el-empty>
     <el-divider></el-divider>
+    <el-switch
+      v-model="value1"
+      active-text="按月付费"
+      inactive-text="按年付费">
+    </el-switch>
+    <el-switch
+      style="display: block"
+      v-model="value2"
+      active-color="#13ce66"
+      inactive-color="#ff4949"
+      active-text="按月付费"
+      inactive-text="按年付费">
+    </el-switch>
   </div>
 </template>
 
@@ -11,6 +24,8 @@ export default {
   name: 'App',
   data() {
     return {
+      value1: true,
+      value2: true，
       tableData: [{
           date: '2016-05-02',
           name: '王小虎',
